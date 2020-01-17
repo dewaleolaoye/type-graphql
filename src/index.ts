c92956e34732b36deab7210f6ed9fab0b540a2b6
dewaleolaoye <adewaleolaoye21@gmail.com>
2020-01-17 17:24:16 +0100

    register resolver

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-express';
 import * as Express from 'express';
-import { buildSchema, Resolver, Query } from 'type-graphql';
-
-@Resolver()
-class HelloResolver {
-  @Query(() => String)
-  async hello() {
-    return 'hello world';
-  }
-}
+import { buildSchema } from 'type-graphql';
+import { createConnection } from 'typeorm';
+import { RegisterResolver } from './modules/User/Register';
 
 const main = async () => {
+  await createConnection();
   const schema = await buildSchema({
-    resolvers: [HelloResolver],
+    resolvers: [RegisterResolver],
   });
 
   const appolloServer = new ApolloServer({ schema });
